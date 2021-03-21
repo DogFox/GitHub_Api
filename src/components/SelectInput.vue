a912b22a4d60dcb0f77cc35b51ba7c2932d865c1
DogFox <kurovda@gmail.com>
2021-03-21 07:10:33 +0300

    select + btn

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <div class="label">
+      {{ label }}
+    </div>
+    <select class="select" :value="value" @change="onChange($event)">
+      <option v-for="item in items" v-bind:key="item.value">
+        {{ item[itemText] }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+  props: {
+    label: { type: String, default: '' },
+    itemText: { type: String, default: 'text' },
+    value: { type: String, default: undefined },
+    items: { type: Array, required: true },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onChange(payload: any) {
+      this.$emit('change', payload.target.value);
+    },
+  },
+});
+</script>
+
+<style>
+.wrapper {
+  padding: 5px 10px 5px 10px;
+  margin: 0px 10px 0px 10px;
+}
+.select {
+  min-width: 200px;
+}
+</style>
---
+++ src/components/SimpleButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <button @click="$emit('click')">{{ text }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    text: { type: String, default: 'Кнопка' },
+  },
+});
+</script>
+
+<style>
+.wrapper {
+  padding: 5px 10px 5px 10px;
+  margin: 0px 10px 0px 10px;
+}
+</style>
