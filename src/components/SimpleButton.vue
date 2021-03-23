a5f7616fbcbda74b7c6478b46097a59cb8005d3e
DogFox <kurovda@gmail.com>
2021-03-24 00:16:45 +0300

    fix styles

--- src/components/SimpleButton.vue
+++ src/components/SimpleButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <button @click="$emit('click')">{{ text }}</button>
+    <button class="button" @click="$emit('click')">{{ text }}</button>
   </div>
 </template>
 
